66b8b81813fd07f37de42a7cb219291fa118f7b3
Dipu <dipukumari359@gmail.com>
2025-12-13 13:32:27 +0100

    fix(v2): switch to IndexedDB to avoid quota errors with large vocabulary

--- v2-react/src/store/useAppStore.js
+++ v2-react/src/store/useAppStore.js
@@ -1,5 +1,22 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
+import { get, set, del } from 'idb-keyval';
+
+// Custom Storage Engine for IndexedDB
+const storage = {
+    getItem: async (name) => {
+        // console.log(name, 'has been retrieved');
+        return (await get(name)) || null;
+    },
+    setItem: async (name, value) => {
+        // console.log(name, 'with value', value, 'has been saved');
+        await set(name, value);
+    },
+    removeItem: async (name) => {
+        // console.log(name, 'has been deleted');
+        await del(name);
+    },
+};
 
 export const useAppStore = create(
     persist(
@@ -59,7 +76,7 @@ export const useAppStore = create(
         }),
         {
             name: 'lingoflow-storage', // unique name
-            storage: createJSONStorage(() => localStorage),
+            storage: createJSONStorage(() => storage),
         }
     )
 );
